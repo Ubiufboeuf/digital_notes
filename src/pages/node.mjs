f71e78c455be612cb1206ae99bfdc774fd34ecb6
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2024-08-30 00:27:05 -0300

    le herré de código y ahora hace falta otro push

--- src/pages/node.mjs
+++ src/pages/node.mjs
@@ -1,5 +1,5 @@
-// import os from "node:os"
-// import fs from "node:fs"
+import os from "node:os"
+import fs from "node:fs"
 
-// export const $os = () => os
-// export const $fs = () => fs
+export const $os = () => os
+export const $fs = () => fs
