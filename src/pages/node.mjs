6eaf6af91a99ad084f93c23c50bbf1f38e32ed56
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2024-08-30 10:47:46 -0300

    Pruebas con node:os

--- src/pages/node.mjs
+++ src/pages/node.mjs
@@ -1,23 +1,7 @@
+import process from "node:process"
 import os from "node:os"
 import fs from "node:fs"
 
+export const $process = process
 export const $os = os
 export const $fs = fs
-
-export function principalDir() {
-  fs.readdir(`${os.homedir()}/`, (err, data) => {
-    if (err) throw new err();
-    // console.log("data: ", data)
-    try {
-      const l = [];
-      for (const i of data) {
-        l.push(i);
-      }
-      // console.log("l: ", l);
-      // console.log(data);
-      return l;
-    } catch (e) {
-      console.error(e);
-    }
-  })
-}
