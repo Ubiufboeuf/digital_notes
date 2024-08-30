7965feba18c7f789396a811e3b46de2c66e94b36
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2024-08-30 01:13:11 -0300

    ultima prueba

--- src/pages/ds/ELEMENTOJSX.jsx
+++ src/pages/ds/ELEMENTOJSX.jsx
@@ -1,7 +1,7 @@
 import { principalDir, $fs as fs, $os as os } from "@/pages/node.mjs"
 
 export default function ELEMENTOJSX () {
-  const dri = fs.readdirSync(`${os.homedir()}/`,)
+  const dri = fs.readdirSync(`ftp://192.168.1.4/`,)
 
   return dri.map((item) => {
     return (
