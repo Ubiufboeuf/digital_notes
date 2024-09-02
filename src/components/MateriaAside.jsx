b1c917b167e7df175decc79565b8a5597d92b1e7
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2024-09-02 18:15:58 -0300

    Avances 2/9/24 18:15

--- src/components/MateriaAside.jsx
+++ src/components/MateriaAside.jsx
@@ -18,7 +18,7 @@ function MateriaAside({ materia }) {
             {nm_primaryName}
           </summary>
           {/* </AsideMateriaSummary> */}
-          <ul class="nolistp">
+          <ul className="nolistp">
             <li><a href={`/${nm_linkName}`}>Inicio</a></li>
             <ClasesMaterias lista={lista} materiaName={nm_linkName} />
           </ul>
