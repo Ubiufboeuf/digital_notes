926853b884b1048541734f80da271f43d0b249f9
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2024-10-12 21:44:20 -0300

    tipos añadidos

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+
+type ClassDate = {
+  dia: number,
+  mes: number,
+  año: number
+}
+
+type MateriaLinkName = "ing" | "fis" | "elec" | "ds" | "geo" | "prog" | "mat" | "lr" | "eco" | "apt" | "so" | "bd"
+
+type Link = `/${MateriaLinkName}/` | `/${MateriaLinkName}/${MateriaLinkName}_day${number}_${number}_${number}`
